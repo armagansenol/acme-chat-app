34c0ac1d17ade4dfdf2acd62dcbbb4142add0055
armagansenol <armagansnl@gmail.com>
2024-08-24 18:52:50 +0300

    update global variabled

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -3,6 +3,12 @@ import { css } from "styled-components"
 
 export const variables = css`
   :root {
-    --bg: ${grayDark.gray1};
+    --bg-1: ${grayDark.gray1};
+    --bg-2: ${grayDark.gray2};
+    --bg-3: ${grayDark.gray3};
+
+    --text-1: ${grayDark.gray12};
+    --text-2: ${grayDark.gray11};
+    --text-3: ${grayDark.gray10};
   }
 `
