7c47ba7c1ceb8391efe130dbdd268d492afa3bce
armagansenol <armagansnl@gmail.com>
2024-08-24 16:33:17 +0300

    add layout components

---
+++ src/components/ui/layout/index.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+export const Flex = styled.div`
+  display: flex;
+`
+
+export const Container = styled.div`
+  width: "100vw";
+  height: "100%";
+  display: "flex";
+  flex-direction: "column";
+  align-items: "stretch";
+  overflow: "hidden";
+`
+
+export const Box = styled.div``
+
+export const BoxAbsolute = styled.div`
+  position: absolute;
+`
