13eca8846cffa3f7ea7816dadd0dbdf90f5d3afe
armagansenol <armagansnl@gmail.com>
2024-08-25 15:20:00 +0300

    remove curly brackets

--- src/components/chat-window/ChatWindow.tsx
+++ src/components/chat-window/ChatWindow.tsx
@@ -3,8 +3,8 @@ import { StyledChatWindow, StyledMessageContainer } from "./styles"
 import { AnimatePresence } from "framer-motion"
 
 import { ChatMessage } from "@/components/chat-message"
-import { MessageProps } from "@/types"
 import { useScrollToElement } from "@/hooks/useScroll"
+import { MessageProps } from "@/types"
 
 export interface ChatWindowProps {
   messages: MessageProps[]
@@ -17,19 +17,17 @@ export default function ChatWindow(props: ChatWindowProps) {
 
   return (
     <>
-      {
-        <StyledChatWindow>
-          <AnimatePresence>
-            {messages.map((message, i) => {
-              return (
-                <StyledMessageContainer key={i} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-                  <ChatMessage {...message} />
-                </StyledMessageContainer>
-              )
-            })}
-          </AnimatePresence>
-        </StyledChatWindow>
-      }
+      <StyledChatWindow>
+        <AnimatePresence>
+          {messages.map((message, i) => {
+            return (
+              <StyledMessageContainer key={i} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                <ChatMessage {...message} />
+              </StyledMessageContainer>
+            )
+          })}
+        </AnimatePresence>
+      </StyledChatWindow>
       <div style={{ visibility: "hidden" }} ref={chatEndRef} />
     </>
   )
