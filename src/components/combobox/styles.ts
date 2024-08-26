412800a2f5314eeaef5f939b36a6be8f3b60d80b
armagansenol <armagansnl@gmail.com>
2024-08-26 22:10:15 +0300

    update styles for message content

--- src/components/combobox/styles.ts
+++ src/components/combobox/styles.ts
@@ -1,30 +1,40 @@
 import { Flex } from "@/components/ui/layout"
+import { hover } from "@/styles/misc"
 import { desktop, desktopVW, mobileVW } from "@/styles/responsive"
 import styled, { css } from "styled-components"
 
 export const StyledCombobox = styled(Flex)`
-  height: ${mobileVW(500)};
-  width: ${mobileVW(500)};
-
-  ${desktop(css`
-    height: ${desktopVW(500)};
-    width: ${desktopVW(500)};
-  `)}
+  justify-content: flex-start;
 `
 
 export const StyledOptionsContainer = styled(Flex)`
-  flex-direction: column;
-  align-items: stretch;
   gap: ${mobileVW(20)};
-  padding: ${mobileVW(20)} 0;
+  padding: ${mobileVW(20)};
+  flex-wrap: wrap;
 
   ${desktop(css`
     gap: ${desktopVW(20)};
-    padding: ${desktopVW(20)} 0;
+    padding: ${desktopVW(20)};
   `)}
 `
 
 export const StyledOption = styled(Flex)`
+  border: 1px solid var(--text-2);
   align-items: center;
+  border-radius: ${mobileVW(32)};
   cursor: pointer;
+  padding: ${mobileVW(0)} ${mobileVW(12)};
+
+  ${hover(css`
+    &:hover {
+      background-color: var(--text-2);
+      color: var(--bg-1);
+    }
+  `)}
+
+  ${desktop(css`
+    border-radius: ${desktopVW(32)};
+    gap: ${desktopVW(30)};
+    padding: ${desktopVW(4)} ${desktopVW(20)};
+  `)}
 `
