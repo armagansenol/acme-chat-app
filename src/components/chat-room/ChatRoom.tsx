e9527596d6271dd9d59d5afe24dfa83389b9e90f
armagansenol <armagansnl@gmail.com>
2024-08-26 13:39:37 +0300

    add welcome message

--- src/components/chat-room/ChatRoom.tsx
+++ src/components/chat-room/ChatRoom.tsx
@@ -9,10 +9,27 @@ import {
 import { ChatInput } from "@/components/chat-input"
 import { ChatWindow } from "@/components/chat-window"
 import { ScrollArea } from "@/components/ui/scroll-area"
+import useMessagePayload from "@/hooks/useMessagePayload"
 import { useChatReducer } from "@/store/reducers/chat-reducer"
+import { useEffect, useRef } from "react"
 
 export default function ChatRoom() {
   const [state, dispatch] = useChatReducer()
+
+  const initialized = useRef(false)
+  const { createTextMessage } = useMessagePayload({
+    type: "ADD_MESSAGE",
+    incoming: true,
+  })
+
+  useEffect(() => {
+    if (!initialized.current) {
+      initialized.current = true
+
+      const messagePayload = createTextMessage("Welcome to Acme Chat!")
+      dispatch(messagePayload)
+    }
+  }, [createTextMessage, dispatch])
 
   return (
     <StyledChatRoom>
