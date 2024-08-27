a491394757f0720a5c6857105974cdaaa04529f0
armagansenol <armagansnl@gmail.com>
2024-08-27 09:22:53 +0300

    update responsive styles

--- src/components/combobox/styles.ts
+++ src/components/combobox/styles.ts
@@ -8,32 +8,39 @@ export const StyledCombobox = styled(Flex)`
 `
 
 export const StyledOptionsContainer = styled(Flex)`
-  gap: ${mobileVW(20)};
-  padding: ${mobileVW(20)};
+  gap: ${mobileVW(10)};
+  padding: ${mobileVW(4)} ${mobileVW(0)};
   flex-wrap: wrap;
+  flex-direction: column;
+  align-items: start;
 
   ${desktop(css`
-    gap: ${desktopVW(20)};
+    flex-direction: row;
+    gap: ${desktopVW(10)};
     padding: ${desktopVW(20)};
   `)}
 `
 
 export const StyledOption = styled(Flex)`
-  border: 1px solid var(--text-2);
+  border: 1px solid var(--text-3);
   align-items: center;
   border-radius: ${mobileVW(32)};
   cursor: pointer;
-  padding: ${mobileVW(0)} ${mobileVW(12)};
+  font-size: ${mobileVW(12)};
+  line-height: 1.25;
+  padding: ${mobileVW(4)} ${mobileVW(10)};
+  text-align: center;
 
   ${hover(css`
     &:hover {
-      background-color: var(--text-2);
+      background-color: var(--text-3);
       color: var(--bg-1);
     }
   `)}
 
   ${desktop(css`
     border-radius: ${desktopVW(32)};
+    font-size: ${desktopVW(15)};
     gap: ${desktopVW(30)};
     padding: ${desktopVW(4)} ${desktopVW(20)};
   `)}
